b8ba870c2527762905bf50f71d5b2e1482059024
David Huerta <david@haystackproject.com>
2012-10-17 16:54:50 -0400

    bananas added to things index

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -6,7 +6,7 @@ class VotesController < ApplicationController
       @sms_response = nil
       
       if incoming_message == 'HELP'
-        @sms_response = I18n.t :help, :thing => 'flavor', :help_website => 'http://youtube.com/watch?v=YQ4j-MBnLQo' 
+        @sms_response = I18n.t :help, :thing => 'banana', :help_website => 'http://youtube.com/watch?v=YQ4j-MBnLQo' 
       else
         @user = User.find_by_phone(phone_number)
         
@@ -27,28 +27,35 @@ class VotesController < ApplicationController
           if Time.now > Time.parse(Settings.contest_start)
             @sms_response = I18n.t 'post_contest_message'
           elsif Time.now < Time.parse(Settings.contest_end)
-            @sms_response = I18n.t 'pre_contest_message', :thing => 'flavor', :start_date => Time.parse(Settings.contest_start).strftime("%b %d")
+            @sms_response = I18n.t 'pre_contest_message', :thing => 'banana', :start_date => Time.parse(Settings.contest_start).strftime("%b %d")
           else
             if incoming_message =~ /^[0-9]{5}/
               @thing = Thing.find_by_vote_code(incoming_message)
               
               if @thing.nil?
-                @sms_response = I18n.t 'invalid_code', :thing => 'flavor'
+                @sms_response = I18n.t 'invalid_code', :thing => 'banana'
               else
-                @vote = Vote.new
-                @vote.user_id
-                @vote.voted_at = DateTime.now
+                #todo: check for dupes
+                @vote = Vote.find_by_user_id(@user.id)
                 
-                if @vote.save
-                  # render some xml and stuff
-                  @sms_response = I18n.t 'voted'
-                else
-                  logger.error "Vote failed for user id: #{ @user.id }"
+                if @vote.nil?
+                  @vote = Vote.new
+                  @vote.user_id = @user.id
+                  @vote.voted_at = DateTime.now
+                  
+                  if @vote.save
+                    # render some xml and stuff
+                    @sms_response = I18n.t 'voted'
+                  else
+                    logger.error "Vote failed for user id: #{ @user.id }"
+                  end
+                else #dupe!
+                  @sms_response = I18n.t 'already_voted'
                 end
               end
             else
               # invalid
-              @sms_response = I18n.t 'invalid_code', :thing => 'flavor'
+              @sms_response = I18n.t 'invalid_code', :thing => 'banana'
             end
           end
         end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,5 @@
 class Vote < ActiveRecord::Base
   belongs_to :user
   #include ActiveModel::Validations
+  
 end
